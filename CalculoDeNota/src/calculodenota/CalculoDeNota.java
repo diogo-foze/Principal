07fa9828cc67456f8ec88f2135ff1b47888dff9f
Diogo Soares <diogo@foze.com.br>
2018-07-30 12:55:12 -0300

    organização pra melhor aprendizado e comentarios

--- CalculoDeNota/src/calculodenota/CalculoDeNota.java
+++ CalculoDeNota/src/calculodenota/CalculoDeNota.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package calculodenota;
-
-import java.util.Scanner;
-
-/**
- *
- * @author Diogo
- */
+package calculodenota;      //nome do pacote
+import java.util.Scanner;   //importar a biblioteca
 public class CalculoDeNota {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         //declaração das variaveis
@@ -23,8 +9,8 @@ public class CalculoDeNota {
         //Instanciar Scanner e atribuir a variavel scan
         Scanner scan =  new Scanner(System.in);
         System.out.println("Digite a nota da primeira prova: ");//exibe a mensagem para o usuário
-        nota1 = scan.nextDouble();// atribui o numero digitado na variavel
-        System.out.println("Valor da nota1: "+nota1);//exibe ao usuário o valor da nota1 
+        nota1 = scan.nextDouble();                              // atribui o numero digitado na variavel
+        System.out.println("Valor da nota1: "+nota1);           //exibe ao usuário o valor da nota1 
     }
     
 }
