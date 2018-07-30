82a33ae7f02e0d8b6125d8c49072317a838c50a2
Diogo Soares <diogo@foze.com.br>
2018-07-30 13:20:20 -0300

    inicio do trabalho

---
+++ Principal/src/principal/Principal.java
@@ -0,0 +1,15 @@
+package principal;
+/**
+ *
+ * @author Diogo
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
