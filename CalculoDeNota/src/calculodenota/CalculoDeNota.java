97d86df7c9c7ab6f5379a773b42678268c565416
Diogo Soares <diogo@foze.com.br>
2018-07-30 13:09:03 -0300

    pequenas correções e adicionar os comentários para fixar o aprendizado

--- CalculoDeNota/src/calculodenota/CalculoDeNota.java
+++ CalculoDeNota/src/calculodenota/CalculoDeNota.java
@@ -12,17 +12,17 @@ public class CalculoDeNota {
         nota1 = scan.nextDouble();                              // atribui o numero digitado na variavel
         System.out.println("Digite a nota da segunda prova: ");
         nota2 = scan.nextDouble();
-        if((nota1+nota2)/2 < 7){
-            media= (nota1 +nota2)/2;
+        if((nota1+nota2)/2 < 7){    // se a media das provas for menor que 7 o aluno está de final
+            media= (nota1 +nota2)/2; //media recebe a soma das 2 notas e divide por 2 para termos a média das provas
             System.out.println("Recuperação final sua media das provas foi: "+media);
-        }else{
+        }else{  //senão é fará um trabalho com mais uma nota
             System.out.println("Digite a nota do trabalho: ");
             trabalho = scan.nextDouble();
-            media=(nota1+nota2+trabalho)/3;
-            if(trabalho>= 7 ){               
+            media=(nota1+nota2+trabalho)/3; //media recebe a soma das 3 notas e divide por 3 para termos a média
+            if(trabalho>= 7 ){ // se a nota do trabalho for maior o igual a 7 esta aprovado              
                 System.out.println("Parabens você foi aprovado com média: "+media);
-            }else{
-                System.out.println("Você esta de Recuperação sua Meida foi: "+media);
+            }else{  //senão esta de recuperação
+                System.out.println("Você esta de Recuperação sua média foi: "+media);
             }
                 
         }
